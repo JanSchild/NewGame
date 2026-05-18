45aa107672cb9c9663131c18b7f624169da0706a
Jan Schild <jan.schild@proton.me>
2026-05-18 21:23:07 +0200

    more expressive name

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,8 @@
-import { wss } from "./wss.js";
+import { wss as websocketServer } from "./wss.js";
 import { wsLogger } from "./logger.js";
 import { SocketManager } from "./SocketManager.js";
 
-wss.on("connection", (socket) => {
+websocketServer.on("connection", (socket) => {
     wsLogger.info("Client connected");
     let clientId: string = `id-${Math.round(Math.random() * 100)}`;
     wsLogger.info(`Assigned new client ID: ${clientId}`);
